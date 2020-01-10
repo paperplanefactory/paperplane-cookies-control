8178b6af922b9974131cf98f263bf83280777884
paperplanefactory <giovanni@paperplanefactory.com>
2020-01-10 09:47:31 +0100

    updated version

--- paperplane-cookies-control.php
+++ paperplane-cookies-control.php
@@ -3,7 +3,7 @@
 Plugin Name: Paperplane Cookies Control
 Plugin URI: https://github.com/paperplanefactory/paperplane-cookies-control
 description: A plugin to handle cookies and cookies notice banner, GDPR compliant. You need to activate <strong><a href="https://www.advancedcustomfields.com/pro/">ACF PRO</a></strong> to make Paperplane Cookie Control.
-Version: 1.1.4
+Version: 1.1.5
 Author: Paperplane
 Author URI: https://www.paperplanefactory.com
 Copyright: Paperplane
